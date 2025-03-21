031a14ef3cb2fa02d47bd7d95892543212d50c38
Faisal <your-email@example.com>
2025-03-21 19:12:28 +0530

    Day 6 Python Functions and While Loop

---
+++ Day 6 - Escaping the maze (Beginner).py
@@ -0,0 +1,8 @@
+# I completed the day 6 topics that was based on Python Functions and Karel.
+#
+# I did the Reeborg's World (search it in google) problems;
+#     Hurdle 1,2,3,4.
+#
+# Then finally did the project of that day which was on the problem 'Maze'.
+#
+# Learnt about python functions and its usage and while loop and its usage as well.
