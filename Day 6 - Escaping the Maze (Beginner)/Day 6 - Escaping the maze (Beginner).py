e2b1ef907fe5ca8c61dd378f4394a19ed66ac9d5
Faisal <your-email@example.com>
2025-03-22 12:08:44 +0530

    Day 6 Python Functions and While Loop

--- Day 6 - Escaping the Maze (Beginner)/Day 6 - Escaping the maze (Beginner).py
+++ Day 6 - Escaping the Maze (Beginner)/Day 6 - Escaping the maze (Beginner).py
@@ -5,4 +5,4 @@
 #
 # Then finally did the project of that day which was on the problem 'Maze'.
 #
-# Learnt about python functions and its usage and while loop and its usage as well.
+# Learnt and implemented python functions, while loop and for loop as well.
